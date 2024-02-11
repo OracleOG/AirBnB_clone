1fc3589889bc56beaa6d38afef40a92cb7c810e1
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2024-02-11 15:52:17 +0100

    fix checker error on console 1 (1)

--- console.py
+++ console.py
@@ -6,22 +6,12 @@ a module that test the cmd module features of python.
 contains the console for the first part of the Airbnb Clone project
 """
 import cmd
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.amenity import Amenity
-from models.city import City
-from models.review import Review
-from models.state import State
-from models import storage
-import json
 
 
 class HBNBCommand(cmd.Cmd):
     """contains entry point of the
       command interpreter for the Airbnb console"""
     prompt = '(hbnb) '
-
 
     def do_quit(self, line):
         """command exits the console. (works like EOF)"""
